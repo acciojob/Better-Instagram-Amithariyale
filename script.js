52ea035eb17d136c055324329bb4d6c6b9554796
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-01-03 17:22:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 //your code here
-const divs=document.querySelectorAll('image');
+const divs=document.querySelectorAll('.image');
 
 document.addEventListener('DOMContentLoaded',()=>{
 	let i=1;
 	divs.forEach(item=>item.id=`div${i++}`);
+});
+
+div.forEach((item)=>{
+	item.addEventListener('dragstart', function(event) {
+	  this.startX = event.clientX;
+	  this.startY = event.clientY;
+	});
+	
+	item.addEventListener('dragend', function(event) {
+	  const newX = event.clientX;
+	  const newY = event.clientY;
+	
+	  if (this.startX !== newX || this.startY !== newY) {
+		this.style.left = newX + 'px';
+		this.style.top = newY + 'px';
+	  }
+	});
 })
+
